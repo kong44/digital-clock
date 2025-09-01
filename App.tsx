254b3be8615e965430e17d86182aac5399a238a9
kong44 <sokkongchan2@gmail.com>
2025-09-01 17:01:18 +0700

    click play if no play can switch channel

--- App.tsx
+++ App.tsx
@@ -376,24 +376,19 @@ const App: React.FC = () => {
                             </svg>
                        )}
                     </button>
-                    {
-                        isPlaying && (
-                            <button
-                                onClick={handleTrackEnd}
-                                className="text-xs sm:text-sm font-semibold py-1 px-3 rounded-full border hover:bg-gray-700/50 transition-all focus:outline-none focus:ring-2 flex items-center justify-center"
-                                style={buttonStyle}
-                                aria-label={isPlaying ? "Pause music" : "Play music"}
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                    <polyline points="16 3 21 3 21 8"></polyline>
-                                    <line x1="4" y1="20" x2="21" y2="3"></line>
-                                    <polyline points="16 17 21 17 21 22"></polyline>
-                                    <line x1="4" y1="4" x2="11" y2="11"></line>
-                                </svg>
-                            </button>
-                        )
-
-                    }
+                    <button
+                        onClick={handleTrackEnd}
+                        className="text-xs sm:text-sm font-semibold py-1 px-3 rounded-full border hover:bg-gray-700/50 transition-all focus:outline-none focus:ring-2 flex items-center justify-center"
+                        style={buttonStyle}
+                        aria-label="switch channel"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <polyline points="16 3 21 3 21 8"></polyline>
+                            <line x1="4" y1="20" x2="21" y2="3"></line>
+                            <polyline points="16 17 21 17 21 22"></polyline>
+                            <line x1="4" y1="4" x2="11" y2="11"></line>
+                        </svg>
+                    </button>
                 </div>
                 
                 <div className="relative z-10 flex flex-col items-center">
